c621a1c9666933c877af9986a3054cab74d3f334
Kaizhe Huang <hkz85825915@gmail.com>
2016-02-13 23:16:44 +1100

    Update purge script

--- purge-cf-cache.sh
+++ purge-cf-cache.sh
@@ -4,4 +4,4 @@ curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$(cat ./cf_zone_id)/p
 	-H "X-Auth-Email: hkz85825915@gmail.com" \
 	-H "X-Auth-Key: $(cat ~/Utils/cf_apikey)" \
 	-H "Content-Type: application/json" \
-	-d '{"files": ["https://pill.fiveyellowmice.com/", "https://pill.fiveyellowmice.com/feed.xml"]}'
+	-d '{"files": ["https://pill.fiveyellowmice.com/", "https://pill.fiveyellowmice.com/feed.xml", "https://pill.fiveyellowmice.com/pages.txt", "https://pill.fiveyellowmice.com/pills.json"]}'
